780417ca98c8702c839f4ba2eda718f7fe6d4f16
Nathan <yuzhong.n.deng@gmail.com>
2020-11-02 21:39:17 -0800

    bert

--- evaluate.py
+++ evaluate.py
@@ -27,6 +27,92 @@ def process_preds(samples, pairwise_preds, indices):
         
     return predictions
 
+
+def collect_data(*paths):
+    """
+    Collect set of relations occurring in given samples.
+    :param paths: Paths of json files containing samples.
+    :return: Set of relation tuples.
+    """
+    # First, we read json samples to learn relations from
+    samples = []
+    for path in paths:
+        with io.open(path, 'r', encoding='utf-8') as f:
+            samples += json.load(f)
+
+    # Collect all the occurring relations
+    relations = set()
+    for sample in samples:
+        entities = sample['entities']
+        for interaction in sample['interactions']:
+            i, j = interaction['participants']
+            for a in entities[i]['names']:
+                for b in entities[j]['names']:
+                    relations.add((a, b))
+                    relations.add((b, a))
+
+    return relations
+
+    
+def predict(sample, relations):
+    text = sample['text']
+
+    interactions = []
+    sample['interactions'] = interactions
+
+    entities = []
+    sample['entities'] = entities
+    entity_registry = dict()
+
+    def find_mentions(entity):
+        start = -1
+        while True:
+            start = text.find(entity, start + 1)
+            if start < 0:
+                break
+            end = start + len(entity)
+            yield start, end
+
+    def register_entity(entity):
+        if entity in entity_registry:
+            return entity_registry[entity]
+
+        idx = len(entities)
+        mentions = list(find_mentions(entity))
+        entities.append({
+            'is_state': False,
+            'label': 'protein',
+            'names': {
+                entity: {
+                    'is_mentioned': True,
+                    'mentions': mentions
+                }
+            },
+            'is_mentioned': True,
+            'is_mutant': False
+        })
+        entity_registry[entity] = idx
+        return idx
+
+    for a, b in relations:
+        if a not in text or b not in text:
+            continue
+        # As the database is symmetric, omit duplicates
+        if a >= b:
+            continue
+
+        # Ensure we have entity registered
+        a_idx = register_entity(a)
+        b_idx = register_entity(b)
+
+        interactions.append({
+            'participants': [a_idx, b_idx],
+            'type': 'bind',
+            'label': 1
+        })
+
+    return sample
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('input_dir', type=str,
@@ -50,8 +136,8 @@ def main():
         data = json.load(f)
 
     X_test, y_test, i_test = prepare_data(input_json_path)
-    model = BERTCustomModel()
-    preds_test = model.test(X_test, y_test)
+    model = BERTCustomModel(model_path)
+    preds_test, fscore = model.test(X_test, y_test)
     predictions = process_preds(data, preds_test, i_test)
     
     with open(output_json_path, 'w') as f:
--- train.py
+++ train.py
@@ -1,138 +1,79 @@
-from __future__ import print_function
-
-import io
 import os
 import json
-import argparse
+from model import BERTCustomModel, ENTITY_SEP_TOKEN
 
+#DATA_DIR = '/Users/ydeng/projects/cs598/hw3/bio-relation-extraction'
+DATA_DIR = '/home/imyaboy888/cs598/hw3/bio-relation-extraction'
 
-def collect_data(*paths):
-    """
-    Collect set of relations occurring in given samples.
-    :param paths: Paths of json files containing samples.
-    :return: Set of relation tuples.
-    """
+def prepare_data(*paths):
     # First, we read json samples to learn relations from
     samples = []
     for path in paths:
-        with io.open(path, 'r', encoding='utf-8') as f:
+        with open(path, 'r', encoding='utf-8') as f:
             samples += json.load(f)
 
     # Collect all the occurring relations
-    relations = set()
-    for sample in samples:
+    triples, labels, indices = [], [], []
+
+    # Collect all the occurring relations
+    for idx, sample in enumerate(samples):
+        gold_relations = [tuple(x['participants']) for x in sample['interactions']]
+        text = sample['text']
+
         entities = sample['entities']
-        for interaction in sample['interactions']:
-            i, j = interaction['participants']
-            for a in entities[i]['names']:
-                for b in entities[j]['names']:
-                    relations.add((a, b))
-                    relations.add((b, a))
+        num_entities = len(entities)
+        for i in range(num_entities):
+            for j in range(i + 1, num_entities):
+                for a in entities[i]['names']:
+                    for b in entities[j]['names']:
+                        triples.append((f'{a} {ENTITY_SEP_TOKEN} {b}', text))
+                        triples.append((f'{b} {ENTITY_SEP_TOKEN} {a}', text))
+                        if (i, j) in gold_relations:
+                            labels.append(1)
+                            labels.append(1)
+                        else:
+                            labels.append(0)
+                            labels.append(0)
+                        indices.append((idx, i, j))
+                        indices.append((idx, i, j))
 
-    return relations
-
-
-def predict(sample, relations):
-    """
-    Predict entities and interactions of given sample
-    w.r.t set of known relations.
-    :param sample: Dict with `id` and `text` as strings
-    :param relations: Set of relation tuples
-    :return: Sample augmented with extracted entities and interactions.
-    """
-    text = sample['text']
-
-    interactions = []
-    sample['interactions'] = interactions
-
-    entities = []
-    sample['entities'] = entities
-    entity_registry = dict()
-
-    def find_mentions(entity):
-        start = -1
-        while True:
-            start = text.find(entity, start + 1)
-            if start < 0:
-                break
-            end = start + len(entity)
-            yield start, end
-
-    def register_entity(entity):
-        if entity in entity_registry:
-            return entity_registry[entity]
-
-        idx = len(entities)
-        mentions = list(find_mentions(entity))
-        entities.append({
-            'is_state': False,
-            'label': 'protein',
-            'names': {
-                entity: {
-                    'is_mentioned': True,
-                    'mentions': mentions
-                }
-            },
-            'is_mentioned': True,
-            'is_mutant': False
-        })
-        entity_registry[entity] = idx
-        return idx
-
-    for a, b in relations:
-        if a not in text or b not in text:
-            continue
-        # As the database is symmetric, omit duplicates
-        if a >= b:
-            continue
-
-        # Ensure we have entity registered
-        a_idx = register_entity(a)
-        b_idx = register_entity(b)
-
-        interactions.append({
-            'participants': [a_idx, b_idx],
-            'type': 'bind',
-            'label': 1
-        })
-
-    return sample
+    print(f"Number of triples: {len(triples)}, labels: {len(labels)}, indices: {len(indices)}")
+    return triples, labels, indices
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input_dir', type=str,
-                        help='Path to directory containing input.json.')
-    parser.add_argument('output_dir', type=str,
-                        help='Path to output directory to write predictions.json in.')
-    parser.add_argument('shared_dir', type=str,
-                        help='Path to shared directory.')
-    args = parser.parse_args()
-
-    # Collect information on known relations
-    self_path = os.path.realpath(__file__)
-    self_dir = os.path.dirname(self_path)
-
-    train_json_path = os.path.join(self_dir, 'data', '1.0alpha7.train.json')
-    dev_json_path = os.path.join(self_dir, 'data', '1.0alpha7.dev.json')
-
-    relations = collect_data(train_json_path, dev_json_path)
-
-    # Read input samples and predict w.r.t. set of relations.
-    input_json_path = os.path.join(args.input_dir, 'input.json')
-    output_json_path = os.path.join(args.output_dir, 'predictions.json')
-
-    with io.open(input_json_path, 'r', encoding='utf-8') as f:
-        data = json.load(f)
+def process_preds(samples, pairwise_preds, indices):
+    interactions = defaultdict(set)
+    for i, (sample_idx, a_idx, b_idx) in enumerate(indices):
+        if pairwise_preds[i]:
+            interactions[sample_idx].add((a_idx, b_idx))
 
     predictions = []
-    for sample in data:
-        sample = predict(sample, relations)
+    for sample_idx, sample in enumerate(samples):
+        sample = sample.copy()
+        sample['interactions'] = []
+        for a_idx, b_idx in interactions[sample_idx]:
+            sample['interactions'].append({
+                'participants': [a_idx, b_idx],
+                'type': 'bind',
+                'label': 1
+            })
         predictions.append(sample)
 
-    with open(output_json_path, 'w') as f:
-        json.dump(predictions, f, indent=True)
+    return predictions
 
+def main():
+
+    # Collect information on known relations
+    train_json_path = os.path.join(DATA_DIR, 'data', '1.0alpha7.train.json')
+    dev_json_path = os.path.join(DATA_DIR, 'data', '1.0alpha7.dev.json')
+
+    X_train, y_train, i_train = prepare_data(train_json_path)
+    X_dev, y_dev, i_dev = prepare_data(dev_json_path)
+
+    model = BERTCustomModel()
+    model.train(X_train, y_train, X_dev, y_dev)
+    model.save()
+    
 
 if __name__ == "__main__":
     main()
